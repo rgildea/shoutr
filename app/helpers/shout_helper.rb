a8da2487f236e7c7bbaad9dd8d195bbf7737a374
Ryan Gildea <ryan.gildea@privy.com>
2020-03-31 15:46:12 -0400

    Refactor shout forms

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,8 +1,16 @@
 module ShoutHelper
+  def shout_form_for(content_type)
+    form_for Shout.new do |form|
+      form.hidden_field(:content_type, value: content_type) +
+        form.fields_for(:content) { |content_form| yield(content_form) } +
+        form.submit("Shout!")
+    end
+  end
+
   def like_button(shout)
     if current_user.liked?(shout)
       link_to "Unlike", unlike_shout_path(shout), method: :delete
-    else 
+    else
       link_to "Like", like_shout_path(shout), method: :post
     end
   end
@@ -12,4 +20,5 @@ module ShoutHelper
       link_to mention, user_path(mention[1..-1])
     end.html_safe
   end
+
 end
