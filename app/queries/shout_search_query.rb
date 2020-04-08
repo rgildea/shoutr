6098d695fb86d1bcd5175bcda9997b07742622d9
Ryan Gildea <ryan.gildea@privy.com>
2020-04-08 12:24:36 -0400

    fix rendering issue with search results

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -4,8 +4,8 @@ class ShoutSearchQuery
   end
 
   def to_relation
-    hits = Sunspot.search([TextShout, PhotoShout]) { fulltext term }.hits
-    Shout.where( content: hits.map { |hit| hit.class_name.constantize.new(id: hit.primary_key) } )
+    Shout.where(id: Shout.search { fulltext term }.hits.map(&:primary_key))
+    # Shout.where( content: hits.map { |hit| hit.class_name.constantize.new(id: hit.primary_key) } )
   end
 
   private
